8d763a877b97daacfd32a63fc7ccf9866c1e13f5
Mohammed Aabed <mohmmed.aabed@gmail.com>
2021-02-27 22:00:38 +0300

    First commit

---
+++ src/Categories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Categories = ({ filterItems, allCategories }) => {
+  return (
+    <div className="btn-container">
+      {allCategories.map((category, index) => {
+        return (
+          <button
+            key={index}
+            className="filter-btn"
+            onClick={() => {
+              filterItems(category);
+            }}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
